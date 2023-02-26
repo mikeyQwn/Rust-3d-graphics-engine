aed144162a0a3e73db55287fc7d776c42ed47ada
mikeyQwn <mikeyQwn@gmail.com>
2023-02-26 19:58:59 +0300

    feat: add default cube mesh and translate method to mesh

--- src/lib/math/mesh.rs
+++ src/lib/math/mesh.rs
@@ -9,4 +9,41 @@ impl Mesh {
     pub fn new(triangles: Vec<Triangle3d>) -> Self {
         Self { triangles }
     }
+
+    pub fn get_cube_mesh() -> Self {
+        Mesh::new(vec![
+            //South
+            Triangle3d::new_from_coordinates(0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0),
+            Triangle3d::new_from_coordinates(0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0),
+            //East
+            Triangle3d::new_from_coordinates(1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0),
+            Triangle3d::new_from_coordinates(1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0),
+            //North
+            Triangle3d::new_from_coordinates(1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0),
+            Triangle3d::new_from_coordinates(1.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0),
+            //West
+            Triangle3d::new_from_coordinates(0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0),
+            Triangle3d::new_from_coordinates(0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0),
+            //Top
+            Triangle3d::new_from_coordinates(0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+            Triangle3d::new_from_coordinates(0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.0),
+            //Bottom
+            Triangle3d::new_from_coordinates(1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0),
+            Triangle3d::new_from_coordinates(1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0),
+        ])
+    }
+
+    pub fn translate(&mut self, dx: f64, dy: f64, dz: f64) {
+        for mut triangle in self.triangles.iter_mut() {
+            triangle.a.x += dx;
+            triangle.a.y += dy;
+            triangle.a.z += dz;
+            triangle.b.x += dx;
+            triangle.b.y += dy;
+            triangle.b.z += dz;
+            triangle.c.x += dx;
+            triangle.c.y += dy;
+            triangle.c.z += dz;
+        }
+    }
 }
